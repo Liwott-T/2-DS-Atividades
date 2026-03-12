79fe791216bd4b767937e8f3fd5da702dade6011
Liwott-T <nicolasadacosta@gmail.com>
2026-03-12 12:07:27 -0300

    Atividade 3 (Array) adicionada

---
+++ atividade3/lista_de_compras.js
@@ -0,0 +1,15 @@
+let lista_compras = ["Batata", "Alface", "Cereal", "Massa de Bolo"];
+lista_compras.push("Macarrão", "Suco de Goiaba", "Leite", "Nescau");
+
+lista_compras.forEach(i => {
+    console.log(i);
+});
+
+
+let cesta_de_frutas = ["Tomate", "Banana", "Melancia", "Goiaba", "Morango", "Amora", "Laranja", "Tangerina"];
+cesta_de_frutas.push("Uva", "Maçã", "Carambola");
+
+cesta_de_frutas.forEach(i => {
+
+    console.log(i);
+});
