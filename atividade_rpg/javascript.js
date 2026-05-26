10a169e47e17a35cc65c9ae51e207f410c090a6c
Liwott-T <nicolasadacosta@gmail.com>
2026-05-26 12:17:50 -0300

    Atividade RPG atualizada (Habilidades e Barras)

--- atividade_rpg/javascript.js
+++ atividade_rpg/javascript.js
@@ -8,6 +8,31 @@ class Personagem {
         this.imagem = imagem;
         this.background = background;
     }
+    ataque_heroi(alvo, habilidade){
+        if (this.mana >= habilidade.custo && this.energia >= habilidade.energia) {
+            alvo.hp = alvo.hp - habilidade.dano;
+            if (habilidade.custo > 0) {
+                this.mana -= habilidade.custo
+                this.energia += 34
+            }
+            if (habilidade.energia > 0) {
+                this.energia -= habilidade.energia
+            }
+        }
+        else {
+            return "Mana ou energia insuficiente";
+        }
+    }
+    ataque_boss(alvo){
+        if (this.energia >= 100){
+            alvo.hp -= 15
+            this.energia += 50
+        }
+        else {
+            this.energia += 50
+        }
+    }
+
 }
 
 class Habilidade {
@@ -16,13 +41,15 @@ class Habilidade {
         this.nome = nome;
         this.dano = dano;
         this.custo = custo;
-        this.energia = energia;
+        this.energia = energia; 
     }
 }
 
 //Instanciar Classes (Criar Objetos)
 
 let id_heroi = 0
+
+let listaHabilidades = [];
 
 do {
     id_heroi = parseFloat(prompt(`Escolha um herói (Insira apenas o número)
@@ -35,16 +62,34 @@ let heroi
 let boss
 switch (id_heroi) {
     case 1:
-        heroi = new Personagem ("Mayreel", "A Besta Divina da Colheita", 10, 30, 0, "assets/mayreel_idle.gif", "assets/gt_background.png")
-        boss = new Personagem ("Lucy", "Besta das Sombras", 10, 0, 50, "assets/shadow_beast.gif", "assets/gt_background.png")
+        heroi = new Personagem ("Mayreel", "A Besta Divina da Colheita", 100, 100, 0, "assets/mayreel_idle.gif", "assets/gt_background.png");
+        boss = new Personagem ("Lucy", "Besta das Sombras", 100, 0, 50, "assets/shadow_beast.gif", "assets/gt_background.png");
+
+        listaHabilidades = [
+        new Habilidade (1, "🍃 Solaris", 4, 0, 0),
+        new Habilidade (2, "🌸 Explosão Floral", 8, 10, 0),
+        new Habilidade (3, "🦙 Transformação", 15, 0, 100),
+        ];
         break
     case 2:
-        heroi = new Personagem ("Hutao", "Diretora da Funerária Wangsheng", 10, 30, 0, "assets/hutao_idle.gif", "assets/forest.jpeg")
-        boss = new Personagem ("Dvalin", "Dragão do Leste", 10, 0, 50, "assets/dvalin.gif", "assets/forest.jpeg")
+        heroi = new Personagem ("Hutao", "Diretora da Funerária Wangsheng", 100, 100, 0, "assets/hutao_idle.gif", "assets/forest.jpeg");
+        boss = new Personagem ("Dvalin", "Dragão do Leste", 100, 0, 50, "assets/dvalin.gif", "assets/forest.jpeg");
+                
+        listaHabilidades = [
+        new Habilidade (1, "🔥 Lança", 4, 0, 0),
+        new Habilidade (2, "🦋 Guia do Além", 8, 10, 0),
+        new Habilidade (3, "👻 Pacificadora de Espíritos", 15, 0, 100),
+        ];
         break
     case 18092022:
-        heroi = new Personagem ("FNS", "THE MASTERMIND", 500, 1000, 0, "assets/fns.png", "assets/arena-do-aspas.webp")
-        boss = new Personagem ("Aspas???", "Final Boss", 1000, 2024, 50, "assets/aspas.png", "assets/arena-do-aspas.webp")
+        heroi = new Personagem ("FNS", "THE MASTERMIND", 100, 1000, 0, "assets/fns.png", "assets/arena-do-aspas.webp");
+        boss = new Personagem ("Aspas???", "Final Boss", 100, 2024, 50, "assets/aspas.png", "assets/arena-do-aspas.webp");
+                
+        listaHabilidades = [
+        new Habilidade (1, "🗑️ Trash", 5, 0, 0),
+        new Habilidade (2, "📌 Pinada", 10, 10, 0),
+        new Habilidade (3, "🧠 MASTERMIND", 50, 0, 100),
+        ];
         break
 };
 console.log(document.getElementById("nome_heroi"));
@@ -57,14 +102,22 @@ document.getElementById("titulo_chefe").innerText = boss.titulo;
 document.getElementById("imagem_chefe").src = boss.imagem;
 
 let containerBtn = document.getElementById("controles")
-let listaHabilidades = [
-    new Habilidade (1, "🍃 Ataque", 4, 0, 0),
-    new Habilidade (2, "🤓 Skill", 8, 10, 0),
-    new Habilidade (3, "🦙 Supreme", 15, 0, 100),
-];
-
 listaHabilidades.forEach(hab => {
     let btn = document.createElement("button");
     btn.innerText = hab.nome;
+    btn.classList.add("btn", "btn-light");
     containerBtn.appendChild(btn);
+    btn.onclick = () => {
+        heroi.ataque_heroi(boss, hab)
+        boss.ataque_boss(heroi)
+        atualizarTela()
+    }
 });
+
+const atualizarTela = () => {
+    document.getElementById("hp-heroi").value = heroi.hp
+    document.getElementById("mana-heroi").value = heroi.mana
+    document.getElementById("energia-heroi").value = heroi.energia
+    document.getElementById("hp-boss").value = boss.hp
+    document.getElementById("energia-boss").value = boss.energia
+};
